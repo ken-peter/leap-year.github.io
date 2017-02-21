e112fc20e84dae10269911f4353b611c350d6326
ken-peter <Ken Peter>
2017-02-21 16:36:13 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+  // jQuery user interface logic here.
+
+
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
